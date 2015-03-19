2d210000a1d776460a7fec3478978bfde975a096
fengguo118 <fengguo118@gmail.com>
2015-03-19 16:53:21 +0800

    update new code to server

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
 /*
  * 后台管理系统的主程序
  */
- 
+
 var express = require('express');
-var app     = express();
-var bodyparser   = require('body-parser');
+var app = express();
+var bodyparser = require('body-parser');
 var cookieparser = require('cookie-parser');
 
 /*
  * 初始化中间件使用
  */
 
- app.use(bodyparser.urlencoded({extended:false}));
- app.use(bodyparser.json());
- app.use(cookieparser());
- app.use(express.static(__dirname + '/static'));
+app.use(bodyparser.urlencoded({
+	extended: false
+}));
+app.use(bodyparser.json());
+app.use(cookieparser());
+app.use(express.static(__dirname + '/static'));
 
- /*
-  * 如下是接口函数的主题
-  */
-
-
-
-
+/*
+ * 如下是接口函数的主题
+ */
 
 
-  /*
-   * 启动， 监听端口
-   */
 
- app.listen(3000);
 
- console.log("server is starting...!");
+
+
+/*
+ * 启动， 监听端口
+ */
+
+app.listen(3000);
+
+console.log("server is starting...!");
